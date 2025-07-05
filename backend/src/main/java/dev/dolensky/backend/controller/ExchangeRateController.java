def099d9080839610814e7700d66add58502f8ea
ZakyCS <vdolensk@gmail.com>
2025-07-05 23:44:02 +0200

    fix: Missing CORS origin for frontend

--- backend/src/main/java/dev/dolensky/backend/controller/ExchangeRateController.java
+++ backend/src/main/java/dev/dolensky/backend/controller/ExchangeRateController.java
@@ -4,14 +4,12 @@ import dev.dolensky.backend.data.ExchangeRate;
 import dev.dolensky.backend.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/exchange-rates")
 public class ExchangeRateController {
